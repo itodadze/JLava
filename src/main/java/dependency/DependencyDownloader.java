a791e2ba046ed06f8ff4dc7f361cd13809d041bd
Irakli Todadze <itoda20@freeuni.edu.ge>
2023-08-20 13:06:13 +0400

    Add dependency download logic

--- src/main/java/dependency/DependencyDownloader.java
+++ src/main/java/dependency/DependencyDownloader.java
@@ -1,20 +1,60 @@
 package dependency;
 
 import logger.Logger;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static logger.LogMessages.*;
 
 public class DependencyDownloader {
     private final Logger logger;
     private final RepositoryURLManager repositoryUrlManager;
+    private final CloseableHttpClient httpClient;
 
     public DependencyDownloader(Logger logger, RepositoryURLManager repositoryURLManager) {
         this.logger = logger;
         this.repositoryUrlManager = repositoryURLManager;
+        // TODO: Authentication logic may be needed
+        this.httpClient = HttpClients.createDefault();
     }
 
     // TODO: Cache downloaded dependencies
-    public void downloadDependencies(List<String> dependencies) {
-        // dependency download
+    public void download(String dependency) {
+        List<String> repositories = this.repositoryUrlManager.getURLs();
+        Optional<CloseableHttpResponse> response = repositories.stream().flatMap(
+                repository -> {
+                    HttpGet httpget = new HttpGet(repository + "/" + dependency);
+                    try {
+                        CloseableHttpResponse result = this.httpClient.execute(httpget);
+                        int statusCode = result.getStatusLine().getStatusCode();
+                        if (statusCode >= 200 && statusCode < 300) {
+                            return Stream.of(result);
+                        } else {
+                            return Stream.empty();
+                        }
+                    } catch (Exception e) {
+                        return Stream.empty();
+                    }
+                }
+        ).findFirst();
+        if (response.isPresent()) {
+            this.logger.printLine(DEPENDENCY_DOWNLOAD_SUCCESS.string() + ": %s", dependency);
+        } else {
+            this.logger.printLine(DEPENDENCY_NOT_FOUND.string() + ": %s", dependency);
+        }
+    }
+
+    public void close() {
+        try {
+            this.httpClient.close();
+        } catch(Exception e) {
+            this.logger.printLine(HTTP_ERROR.string() + ": %s", e.getMessage());
+        }
     }
 }
--- src/main/java/logger/LogMessages.java
+++ src/main/java/logger/LogMessages.java
@@ -3,7 +3,10 @@ package logger;
 public enum LogMessages {
     FILE_PATH_NOT_FOUND("Non-existing file path"),
     FILE_COMPILATION_SUCCESS("Files compiled successfully"),
-    FILE_COMPILATION_FAILURE("File compilation error");
+    FILE_COMPILATION_FAILURE("File compilation error"),
+    HTTP_ERROR("Http error"),
+    DEPENDENCY_NOT_FOUND("Could not download dependency"),
+    DEPENDENCY_DOWNLOAD_SUCCESS("Dependency downloaded successfully");
 
     private final String message;
 
